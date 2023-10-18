70c50cb119c6de0281ed8215224371578efecc31
manvi <manvi@Manvitha>
2023-10-19 03:54:25 +0530

    new commit print

--- src/PrintPerWish/PrintMain.java
+++ src/PrintPerWish/PrintMain.java
@@ -1,10 +1,23 @@
 package PrintPerWish;
 
+import java.util.Scanner;
+
 public class PrintMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		PrintFunc1 pf1=new PrintFunc1();
+		PrintFunc2 pf2=new PrintFunc2();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("What do you wish to print if want Good Morning type 1 or type 2 for Good Evening");
+        int n=sc.nextInt();
+        if(n==1)
+        {
+        	pf1.display();
+        }
+        else
+        {
+        	pf2.display();
+        }
 	}
 
 }
